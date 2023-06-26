0cde371a20fa13bd673da03ee6d0e5cf0a128788
danielamansolino <danielamosqueragranda@gmail.com>
2023-06-26 15:18:00 -0400

    items controllers and routes add

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ const port = process.env.PORT || 3001;
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'));
 
+// Protect the api routes below from anonymous users
+const ensureLoggedIn = require('./config/ensureLoggedIn');
+app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
+
+// reviews
+
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
 app.get('/*', function(req, res) {
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from 'react';
+import * as itemsAPI from '../../utilities/items-api'
+
 export default function NewOrderPage() {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      setMenuItems(items);
+    }
+    getItems();
+  }, []);
+
     return (
       <h1>NewOrderPage</h1>
     );
