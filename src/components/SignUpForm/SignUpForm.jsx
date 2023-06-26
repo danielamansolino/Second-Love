f8f43e25c0b8440a04862f1427ee62aebdacdbf9
danielamansolino <danielamosqueragranda@gmail.com>
2023-06-26 10:15:30 -0400

    Sign-in and Sign-up css complete

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -57,14 +57,14 @@ export default class SignUpForm extends Component {
           <div>
               <div className="form-container">
                   <form autoComplete="off" onSubmit={this.handleSubmit}>
-                      <label>Name</label>
-                      <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
-                      <label>Email</label>
-                      <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-                      <label>Password</label>
-                      <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-                      <label>Confirm</label>
-                      <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+                      <label>Name</label> <br/>
+                      <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required /><br/>
+                      <label>Email</label><br/>
+                      <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required /><br/>
+                      <label>Password</label><br/>
+                      <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required /><br/>
+                      <label>Confirm</label><br/>
+                      <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required /><br/>
                       <button type="submit" disabled={disable}>SIGN UP</button>
                   </form>
               </div>
